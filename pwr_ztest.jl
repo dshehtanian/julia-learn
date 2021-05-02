390f63848daa2f67b6619c9d03fa8d6ecdae7eed
dshehtanian <dshehtanian@gmail.com>
2021-05-02 09:39:49 -0700

    updated pwr_ztest.jl

--- pwr_ztest.jl
+++ pwr_ztest.jl
@@ -5,3 +5,13 @@ using Distributions
 function power_norm(n, alpha, mu_0, mu_a, sigma)
     power_calc = 1 - cdf(Normal(0,1),quantile(Normal(0,1),1-alpha)+(mu_0-mu_a)*sqrt(n)/sigma)
 end
+
+function samplesize_norm(power, alpha, mu_0, mu_a, sigma)
+    sample_size = 0
+end
+
+function power_graph(n, alpha, mu_0, mu_a, sigma)
+    for i=1:n
+    power_calc = 1 - cdf(Normal(0,1),quantile(Normal(0,1),1-alpha)+(mu_0-mu_a)*sqrt(n)/sigma)
+    end
+end
